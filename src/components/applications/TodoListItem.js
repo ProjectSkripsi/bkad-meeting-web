d0f368136c1cdfab1907fb04a4a52660b57811bb
arul21 <arul.21@hotmail.com>
2023-01-17 14:42:20 +0700

    compoenents

---
+++ src/components/applications/TodoListItem.js
@@ -0,0 +1,301 @@
+import React, { useState } from 'react';
+import {
+  Card,
+  CardBody,
+  Badge,
+  ButtonDropdown,
+  DropdownToggle,
+  DropdownItem,
+  DropdownMenu,
+  Button,
+  CardFooter,
+  CardHeader,
+  List,
+  CardTitle,
+  CardText,
+} from 'reactstrap';
+import QRCode from 'qrcode.react';
+import Linkify from 'react-linkify';
+import { NavLink, Link } from 'react-router-dom';
+import moment from 'moment';
+import 'moment/locale/id';
+import { Colxx } from '../common/CustomBootstrap';
+import { get } from 'lodash';
+import { adminRoot } from '../../constants/defaultValues';
+import ModalInvite from '../../views/app/Event/ModalInvite';
+import ModalNoTulen from '../../views/app/Event/ModalNoTulen';
+import ModalResult from '../../views/app/Event/ModalEventResult';
+
+moment.locale('id');
+
+const TodoListItem = ({
+  item,
+  deleteData,
+  onUpdateProgress,
+  className,
+  onProccess,
+  onSaveNoTulen,
+  onCancel,
+  setSelectedOption,
+  selectedOption,
+}) => {
+  const [dropdownBasicOpen, setDropdownBasicOpen] = useState(false);
+  const [modalInvite, setModalInvite] = useState(false);
+  const [dataMember, setDataMember] = useState([]);
+  const [titleModal, setTitleModal] = useState('');
+  const [modalNoTulen, setModalNoTulen] = useState(false);
+  const [modalResult, setModalResult] = useState(false);
+  const [tanggal, setTanggal] = useState('');
+  const [isBidang, setIsBidang] = useState(false);
+
+  const renderItem = (data) => {
+    return (
+      <List type='unstyled'>
+        <ul>
+          {data.map((i, index) => (
+            <li key={index}>{i.name}</li>
+          ))}
+        </ul>
+      </List>
+    );
+  };
+
+  const downloadQRCode = (value) => {
+    // Generate download with use canvas and stream
+    const canvas = document.getElementById('qr-gen');
+    const pngUrl = canvas
+      .toDataURL('image/png')
+      .replace('image/png', 'image/octet-stream');
+    let downloadLink = document.createElement('a');
+    downloadLink.href = pngUrl;
+    downloadLink.download = `${value}.png`;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
+
+  const onViewodal = (type, data, tanggal) => {
+    if (type === 'count') {
+      setIsBidang(true);
+      setDataMember(data);
+      setTitleModal('Data Absensi Meeting');
+      setModalInvite(true);
+    } else {
+      setIsBidang(false);
+      setModalInvite(true);
+      setTitleModal('Peserta Meeting Hadir');
+      setDataMember(data);
+      setTanggal(tanggal);
+    }
+  };
+
+  const activeCheck = (date) => {
+    const isBefore = moment(date).isAfter(Date.now());
+    return isBefore;
+  };
+
+  const endDatePlusOne = (date) => {
+    const dates = moment(date).add(1, 'days');
+    const isBefore = moment(dates).isAfter(Date.now());
+    return isBefore;
+  };
+
+  return (
+    <Colxx xxs='12' className='mb-3'>
+      <Card className={className}>
+        <CardBody>
+          <div className='d-flex flex-row mb-3 justify-content-between'>
+            <div className='flex-grow-1'>
+              <p className='font-weight-medium mb-0 '>
+                <CardTitle>
+                  {item.name} {'     '} {endDatePlusOne(item.end)}
+                  <Badge
+                    color={
+                      activeCheck(item.end) && item.status === 'active'
+                        ? 'primary'
+                        : item.status === 'active' && !activeCheck(item.end)
+                        ? 'secondary'
+                        : 'danger'
+                    }
+                    pill
+                  >
+                    {item.status === 'active' && activeCheck(item.end)
+                      ? 'Upcoming'
+                      : item.status === 'active' && !activeCheck(item.end)
+                      ? 'Expired'
+                      : 'Cancel'}
+                  </Badge>
+                </CardTitle>
+                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+              </p>
+            </div>
+
+            <div className='comment-likes'>
+              <ButtonDropdown
+                direction='left'
+                isOpen={dropdownBasicOpen}
+                toggle={() => setDropdownBasicOpen(!dropdownBasicOpen)}
+              >
+                <DropdownToggle caret size='xs' color='secondary' outline>
+                  Actions
+                </DropdownToggle>
+                <DropdownMenu>
+                  {endDatePlusOne(item.end) && (
+                    <DropdownItem onClick={() => setModalNoTulen(true)}>
+                      NoTulen
+                    </DropdownItem>
+                  )}
+                  {item.status === 'active' && activeCheck(item.end) && (
+                    <DropdownItem onClick={() => onCancel(item._id)}>
+                      Batalkan
+                    </DropdownItem>
+                  )}
+                  {item.noTulens.length > 0 && (
+                    <DropdownItem
+                      onClick={() =>
+                        window.open(`/rekapitulasi/${item._id}`, '_blank')
+                      }
+                    >
+                      Downlaod Notulen
+                    </DropdownItem>
+                  )}
+                  <hr />
+                  <DropdownItem onClick={() => deleteData(item._id)}>
+                    Hapus
+                  </DropdownItem>
+                </DropdownMenu>
+              </ButtonDropdown>
+            </div>
+          </div>
+          <hr />
+          <div className='row'>
+            <div className='col-md-8'>
+              <p>
+                Agenda: <b> {item.agenda || '-'} </b>
+              </p>
+              <p>
+                Jadwal:{' '}
+                <b>
+                  {moment(item.start).format('LLL')} -{' '}
+                  {moment(item.end).format('LLL')}
+                </b>
+              </p>
+              <p>
+                Jumlah Absensi:{' '}
+                <b>
+                  <a
+                    style={{ cursor: 'pointer', color: 'blue' }}
+                    onClick={() => onViewodal('count', item.units)}
+                  >
+                    {item.units.length} Bidang
+                  </a>{' '}
+                </b>
+              </p>
+              {item.admittedParticipant.length > 0 && (
+                <p>
+                  Peserta Hadir:{' '}
+                  <b>
+                    <a
+                      style={{ cursor: 'pointer', color: 'blue' }}
+                      onClick={() =>
+                        onViewodal(
+                          'countAdmit',
+                          item.admittedParticipant,
+                          item.start,
+                        )
+                      }
+                    >
+                      {item.admittedParticipant.length} Peserta{' '}
+                    </a>
+                  </b>
+                </p>
+              )}
+
+              <p>
+                Hasil Meeting:{' '}
+                <b>
+                  {item.noTulens.length > 0 ? (
+                    <a
+                      style={{ cursor: 'pointer', color: 'blue' }}
+                      onClick={() => setModalResult(true)}
+                    >
+                      Lihat
+                    </a>
+                  ) : (
+                    <a>Belum Tersedia</a>
+                  )}
+                </b>
+              </p>
+
+              <p>Info Meeting:</p>
+              <List type='unstyled'>
+                <ul>
+                  <li>
+                    Kategori Meeting:{' '}
+                    <b>{item.meetingRoom.category.toUpperCase()}</b>
+                  </li>
+                  <li>
+                    Ruangan: <b>{item.meetingRoom.room}</b>
+                  </li>
+                  <li>
+                    Catatan:
+                    <b>
+                      {' '}
+                      <Linkify>{item.meetingRoom.description}</Linkify>{' '}
+                    </b>
+                  </li>
+                </ul>
+              </List>
+            </div>
+            <div className='col-md-4 text-right'>
+              <div className='col'>
+                <QRCode
+                  id='qr-gen'
+                  value={item._id}
+                  size={220}
+                  level={'H'}
+                  includeMargin={true}
+                />
+              </div>
+              <Button
+                color='primary'
+                className='mr-5'
+                onClick={() => downloadQRCode(item._id)}
+                disabled={item.status === 'active' && !activeCheck(item.end)}
+              >
+                Download QR Code
+              </Button>
+            </div>
+          </div>
+        </CardBody>
+        <CardFooter className='text-muted'>
+          Dibuat oleh {get(item, 'createdBy.name', '-')} -{' '}
+          {moment(item.createdAt).startOf('second').fromNow()}
+        </CardFooter>
+      </Card>
+      <ModalResult
+        isOpen={modalResult}
+        setIsOpen={setModalResult}
+        data={item?.noTulens}
+      />
+      <ModalNoTulen
+        isOpen={modalNoTulen}
+        setIsOpen={setModalNoTulen}
+        onSaveNoTulen={onSaveNoTulen}
+        data={item}
+        setSelectedOption={setSelectedOption}
+        selectedOption={selectedOption}
+      />
+      <ModalInvite
+        isOpen={modalInvite}
+        setIsOpen={setModalInvite}
+        data={dataMember}
+        titleModal={titleModal}
+        tanggal={tanggal}
+        isBidang={isBidang}
+      />
+    </Colxx>
+  );
+};
+
+export default React.memo(TodoListItem);
---
+++ src/components/complaint/ModalProgress.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Modal, ModalHeader, ModalBody, Badge, List } from 'reactstrap';
+
+import { get, isEmpty } from 'lodash';
+import moment from 'moment';
+
+const ModalProgress = ({
+  setModalProgressOpen,
+  modalProgressOpen,
+  data,
+  onUpdateClassification,
+  onChange,
+}) => {
+  const renderItem = (data) => {
+    if (!isEmpty(data)) {
+      return (
+        <List type="unstyled">
+          <ul>
+            {data.map((item) => (
+              <li key={item._id}>{item.name}</li>
+            ))}
+          </ul>
+        </List>
+      );
+    }
+  };
+
+  return (
+    <Modal
+      isOpen={modalProgressOpen}
+      toggle={() => setModalProgressOpen(!modalProgressOpen)}
+    >
+      <ModalHeader>
+        {data.name} - {data.age} Tahun{' '}
+      </ModalHeader>
+      <ModalBody>
+        <p className="text-muted text-small mb-2">NIK</p>
+        <p className="mb-3">{data.nik}</p>
+        <p className="text-muted text-small mb-2">No Handphone</p>
+        <p className="mb-3">{data.contact}</p>
+        <p className="text-muted text-small mb-2">Alamat</p>
+        <p className="mb-3">{data.address}</p>
+        <p className="text-muted text-small mb-2">Tempat & Tanggal Lahir</p>
+        <p className="mb-3">
+          {data.placeBirth} - {moment(data.dateBirth).format('LL')}
+        </p>
+
+        <p className="text-muted text-small mb-2">Gejala</p>
+        {renderItem(data.symptomsId || [])}
+
+        <p className="text-muted text-small mb-2">Diagnosa</p>
+        <p className="mb-3">
+          <Badge color="outline-secondary" className="mb-1 mr-1" pill>
+            {data.criteriaStatus || '-'}
+          </Badge>
+        </p>
+
+        <p className="text-muted text-small mb-2">Diperiksa </p>
+        <p className="mb-3">
+          {' '}
+          {get(data, 'inputBy.name', '')} -{' '}
+          {moment(data.createdAt).format('LL')}
+        </p>
+      </ModalBody>
+    </Modal>
+  );
+};
+
+export default ModalProgress;
